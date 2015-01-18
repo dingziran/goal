dba60fadd0d0f0dd2b3e639b74b95c750555a102
dingziran <dingziranrr@163.com>
2015-01-18 20:34:01 +0800

    tmp

--- client/app/skill/skill.js
+++ client/app/skill/skill.js
@@ -3,6 +3,52 @@ app.config(function($stateProvider) {
     $stateProvider
         .state('skill', {
             url: "/skill",
-            templateUrl: "app/skill/skill.html"
+            templateUrl: "app/skill/skill.html",
+            resolve:{
+                projects:function(ProjectService){
+                    return ProjectService.getProjects();
+                }
+            },
+            controller:function($scope,$state,$stateParams,projects,ProjectService){
+                $scope.projects=projects;
+                $scope.newProject={};
+                $scope.add=function(project){
+                    ProjectService.createProject(project).then(function(){
+                        $state.transitionTo($state.current, $stateParams, {
+                            reload: true,
+                            inherit: false,
+                            notify: true
+                        });
+                    });
+                };
+                $scope.delete=function(project){
+                    ProjectService.deleteProject(project._id.$oid).then(function(){
+                        $state.transitionTo($state.current, $stateParams, {
+                            reload: true,
+                            inherit: false,
+                            notify: true
+                        });
+                    });
+                }
+            }
         });
+});
+
+app.factory('SkillService',function(MongodbService){
+    var collection="/purpose";
+    var service={
+        getPurpose:function(){
+            var url=MongodbService.getDatabase()+collection+"?"+MongodbService.getApiKey();
+            return MongodbService.get(url);
+        },
+        createPurpose:function(data){
+            var url=MongodbService.getDatabase()+collection+"?"+MongodbService.getApiKey();
+            return MongodbService.post(url,data);
+        },
+        deletePurpose:function(id){
+            var url=MongodbService.getDatabase()+collection+"/"+id+"?"+MongodbService.getApiKey();
+            return MongodbService.delete(url);
+        }
+    };
+    return service;
 });
